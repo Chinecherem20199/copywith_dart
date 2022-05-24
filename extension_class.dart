edb6e69df3c30e93cfcecef5dbf3f4fd78a4e647
Chinecherem Augustina Ugwuanyi <51314161+Chinecherem20199@users.noreply.github.com>
2022-05-24 16:20:59 +0100

    Add files via upload
    extension is used when you don't want to use extends keyword in dart

---
+++ extension_class.dart
@@ -0,0 +1,21 @@
+// extension keywoprd must be used
+
+extension ListAverage on List<int> {
+  double average() {
+    var sum = 0;
+    for (var item in this) {
+      sum += item;
+    }
+    return (sum / this.length);
+  }
+}
+
+extension ListSum on List<int> {
+  int sum() {
+    var sum = 0;
+    for (var item in this) {
+      sum += item;
+    }
+    return sum;
+  }
+}
